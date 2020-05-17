8c025f7a3ccf2765c6957f44ceb835b32a334e91
George England <george.england@navtechradar.co.uk>
2020-05-17 21:15:09 +0100

    updating affiliations

--- GeorgeArbonne/urls.py
+++ GeorgeArbonne/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name = 'logout'),
     path('profile/', user_views.profile, name='accountprofile'),
     path('change_password/',auth_views.PasswordChangeView.as_view(template_name='users/change_password.html',success_url='/'),name='change_password'),
-
 ]

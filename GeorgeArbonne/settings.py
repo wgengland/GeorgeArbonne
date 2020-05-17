82d5c844d3cc909d33ca614d6869c0e4eab62e50
George England <george.england@navtechradar.co.uk>
2020-05-17 21:39:07 +0100

    no debug for production

--- GeorgeArbonne/settings.py
+++ GeorgeArbonne/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'dya(d337dol$a*4m@*zqtz-vz=)jzm+h^7%i!+c^y+-(u!6tqf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["192.168.1.142"]
 
